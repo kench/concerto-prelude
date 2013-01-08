95c8b267733c7feae4af4b82b100b31b93e6c173
Kenley Cheung <ken@lordkenthegreat.com>
2013-01-07 19:18:08 -0500

    Chrome apps do not like inline JavaScript within HTML.  Include initialization inside settings.js.

--- chrome-extension/settings.js
+++ chrome-extension/settings.js
@@ -1,6 +1,8 @@
 goog.provide('concerto.player.Settings');
+goog.provide('concerto.player.pages.Settings');
 
 goog.require('goog.dom');
+goog.require('goog.events');
 
 /**
  * Player Settings.
@@ -49,12 +51,38 @@ concerto.player.Settings.load = function() {
 	this.server_version = localStorage["server_version"];
 }
 
+// Google Chrome Packaged Apps CSP restricts inline scripting.
+({
+	var page = new concerto.player.pages.Settings();
+	goog.events.listen(document, goog.events.EventType.LOAD, this.openEditor, page.load);
+})();
+
+/**
+ * Player Settings Page.
+ *
+ * @constructor
+ */
+concerto.player.pages.Settings = function() {
+	this.el_label_id = goog.dom.query("label[for=mac]");
+	this.el_input_id = goog.dom.query("#mac");
+	this.el_input_url = goog.dom.query("#url");
+	this.el_input_version = goog.dom.query("#version");
+};
+goog.exportSymbol('concerto.player.Settings', concerto.player.Settings);
+
+concerto.player.pages.Settings.load = function() {
+	var handler_label = setIDLabel(this.el_label_id, this.el_input_id);
+	populate_form(this.el_input_id, this.el_input_url, this.el_input_version);
+	handler_label();
+	goog.events.listen(this.el_input_version, goog.events.EventType.CHANGE, funct_label);
+};
+
 /**
  * Set up proper input names for the screen identifier.
  * @param {ELement=} label Label element describing the form input
  * @param {ELement=} input Input element for screen identifier
  */
-concerto.player.Settings.setIDLabel = function(label, input) {
+concerto.player.pages.Settings.setIDLabel = function(label, input) {
 	if (this.server_version == 1)
 	{
 		goog.dom.setTextContent(label, "MAC Address");
@@ -73,7 +101,7 @@ concerto.player.Settings.setIDLabel = function(label, input) {
  * @param {string} url Concerto server URL.
  * @param {number} version Concerto server version.
  */
-concerto.settings.populate_form = function(id, url, version)
+concerto.player.pages.Settings.populate_form = function(id, url, version)
 {
 	goog.dom.setProperties(id, {"value": this.screen_id});
 	goog.dom.setProperties(url, {"value": this.server_url});
