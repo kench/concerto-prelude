242bc1b4b80cfc0f6edfa896d0e10a30de38cdc7
Kenley Cheung <ken@lordkenthegreat.com>
2013-01-07 21:15:22 -0500

    Commit rest of refactoring.  Settings page is still broken.

--- chrome-extension/settings.js
+++ chrome-extension/settings.js
@@ -1,8 +1,4 @@
 goog.provide('concerto.player.Settings');
-goog.provide('concerto.player.pages.Settings');
-
-goog.require('goog.dom');
-goog.require('goog.events');
 
 /**
  * Player Settings.
@@ -50,60 +46,3 @@ concerto.player.Settings.load = function() {
 	this.server_url = localStorage["server_url"];
 	this.server_version = localStorage["server_version"];
 }
-
-// Google Chrome Packaged Apps CSP restricts inline scripting.
-({
-	var page = new concerto.player.pages.Settings();
-	goog.events.listen(document, goog.events.EventType.LOAD, this.openEditor, page.load);
-})();
-
-/**
- * Player Settings Page.
- *
- * @constructor
- */
-concerto.player.pages.Settings = function() {
-	this.el_label_id = goog.dom.query("label[for=mac]");
-	this.el_input_id = goog.dom.query("#mac");
-	this.el_input_url = goog.dom.query("#url");
-	this.el_input_version = goog.dom.query("#version");
-};
-goog.exportSymbol('concerto.player.Settings', concerto.player.Settings);
-
-concerto.player.pages.Settings.load = function() {
-	var handler_label = setIDLabel(this.el_label_id, this.el_input_id);
-	populate_form(this.el_input_id, this.el_input_url, this.el_input_version);
-	handler_label();
-	goog.events.listen(this.el_input_version, goog.events.EventType.CHANGE, funct_label);
-};
-
-/**
- * Set up proper input names for the screen identifier.
- * @param {ELement=} label Label element describing the form input
- * @param {ELement=} input Input element for screen identifier
- */
-concerto.player.pages.Settings.setIDLabel = function(label, input) {
-	if (this.server_version == 1)
-	{
-		goog.dom.setTextContent(label, "MAC Address");
-		goog.dom.setProperties(input, {"placeholder": "0F:DE:AD:BE:EF"});
-	}
-	else if (this.server_version == 2)
-	{
-		goog.dom.setTextContent(label, "Screen ID");
-		goog.dom.setProperties(input, {"placeholder": "1"});
-	}
-}
-
-/**
- * Populate settings form.
- * @param {string} id Screen identifier.  MAC address for version 1, ID number for version 2.
- * @param {string} url Concerto server URL.
- * @param {number} version Concerto server version.
- */
-concerto.player.pages.Settings.populate_form = function(id, url, version)
-{
-	goog.dom.setProperties(id, {"value": this.screen_id});
-	goog.dom.setProperties(url, {"value": this.server_url});
-	goog.dom.setProperties(version, {"value": this.server_version});
-};
---
+++ chrome-extension/settings.page.js
@@ -0,0 +1,61 @@
+goog.provide('concerto.player.pages.Settings');
+
+goog.require('concerto.player.Settings');
+
+/**
+ * Player Settings Page.
+ *
+ * @constructor
+ */
+concerto.player.pages.Settings = function() {
+	this.el_label_id = goog.dom.query("label[for=mac]");
+	this.el_input_id = goog.dom.getElement("mac");
+	this.el_input_url = goog.dom.getElement("url");
+	this.el_input_version = goog.dom.getElement("version");
+};
+goog.exportSymbol('concerto.player.Settings', concerto.player.Settings);
+
+concerto.player.pages.Settings.load = function() {
+	var handler_label = setIDLabel(this.el_label_id, this.el_input_id);
+	populate_form(this.el_input_id, this.el_input_url, this.el_input_version);
+	handler_label();
+	goog.events.listen(this.el_input_version, goog.events.EventType.CHANGE, funct_label);
+};
+goog.exportSymbol('concerto.player.Settings.load', concerto.player.Settings.load);
+
+/**
+ * Set up proper input names for the screen identifier.
+ * @param {ELement=} label Label element describing the form input
+ * @param {ELement=} input Input element for screen identifier
+ */
+concerto.player.pages.Settings.setIDLabel = function(label, input) {
+	if (this.server_version == 1)
+	{
+		goog.dom.setTextContent(label, "MAC Address");
+		goog.dom.setProperties(input, {"placeholder": "0F:DE:AD:BE:EF"});
+	}
+	else if (this.server_version == 2)
+	{
+		goog.dom.setTextContent(label, "Screen ID");
+		goog.dom.setProperties(input, {"placeholder": "1"});
+	}
+}
+
+/**
+ * Populate settings form.
+ * @param {string} id Screen identifier.  MAC address for version 1, ID number for version 2.
+ * @param {string} url Concerto server URL.
+ * @param {number} version Concerto server version.
+ */
+concerto.player.pages.Settings.populate_form = function(id, url, version)
+{
+	goog.dom.forms.setValue(id, this.screen_id);
+	goog.dom.forms.setValue(url, this.server_url);
+	goog.dom.forms.setValue(version, this.server_version);
+};
+
+// Google Chrome Packaged Apps CSP restricts inline scripting.
+(function() {
+	var page = new concerto.player.pages.Settings();
+	goog.events.listen(document, goog.events.EventType.LOAD, page.load);
+})();
